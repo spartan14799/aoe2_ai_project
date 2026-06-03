680e1582ee2d3e64e51275a9ff3e05f4d9064ecb
paranimias <ndceliso@unal.edu.co>
2026-06-03 01:37:16 -0500

    cálculo de entropía lista

--- 02_decision_tree/src/main.rs
+++ 02_decision_tree/src/main.rs
@@ -1,4 +1,5 @@
 use csv::Reader;
+use std::collections::HashMap;
 use std::error::Error;
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
@@ -6,15 +7,51 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
 
     let mut rdr = Reader::from_path("data.csv")?;
 
-    let headers = rdr.headers()?;
-    println!("Hay {} columnas.", headers.len());
+    let mut vector_ganadores: Vec<String> = Vec::new();
 
-    if let Some(result) = rdr.records().next() {
+    for result in rdr.records() {
         let record = result?;
-        println!("Primera partida (fila 1): {:?}", record);
+
+        if let Some(ganador) = record.get(8) {
+            vector_ganadores.push(ganador.to_string());
+        }
     }
 
-    println!("¡Lectura exitosa!");
+    println!(
+        "Se cargaron {} registros de la columna objetivo.",
+        vector_ganadores.len()
+    );
+
+    let entropia_total = calcular_entropia(&vector_ganadores);
+    println!("La entropía total de los datos es: {:.4}", entropia_total);
 
     Ok(())
+}
+
+fn calcular_entropia(datos: &[String]) -> f64 {
+    let total_elementos = datos.len() as f64;
+    if total_elementos == 0.0 {
+        return 0.0;
+    }
+
+    // La etiqueta es el mismo dato
+    let mut conteos = HashMap::new();
+    for etiqueta in datos {
+        // Primero consigue el dato de cuantos, 0 o 1
+        let tracker = conteos.entry(etiqueta).or_insert(0);
+        *tracker += 1;
+    }
+
+    // Fórmula de la entropía
+    let mut entropia = 0.0;
+    for &conteo in conteos.values() {
+        let probabilidad = conteo as f64 / total_elementos;
+        // Entropía = 0.0 - probabilidad * log_2(probabilidad)
+        entropia -= probabilidad * probabilidad.log2();
+    }
+    entropia
+}
+
+fn calcular_ganancia() -> &str {
+    return "hola";
 }
